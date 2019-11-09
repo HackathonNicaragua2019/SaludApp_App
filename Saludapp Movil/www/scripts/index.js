ea8aa1cf85eee17d760fc7e34656abd6151319bf
AdminCarlos <carlosflores92.cf@gmail.com>
2019-11-09 14:48:33 -0600

    feat: Implementadas funciones ajax para conexion con la base de datos.

--- Saludapp Movil/www/scripts/index.js
+++ Saludapp Movil/www/scripts/index.js
@@ -35,6 +35,55 @@ function initializeControls() {
     $("#buttonObtenerNota").click(function() {
         obtenerNotaIngreso();        
     });
-
 }
 
+function obtenerNotaIngreso() {
+
+    $("#tableNotaIngreso1era").empty();
+
+    $.ajax({
+
+        type: "POST",
+        url: "http://192.168.1.100/Saludapp/BackendFolder/consultarNotaIngreso1era.php",
+        data: {},
+        cache: false,
+        success: function (data) {
+            $("#tableNotaIngreso1era").replaceWith(data);
+        },
+        error: function (data) {
+            alert("No hay conexion con la base de datos");
+        }
+
+    });
+
+    $.ajax({
+
+        type: "POST",
+        url: "http://192.168.1.100/Saludapp/BackendFolder/consultarNotaIngreso2da.php",
+        data: {},
+        cache: false,
+        success: function (data) {
+            $("#tableNotaIngreso2da").replaceWith(data);
+        },
+        error: function (data) {
+            alert("No hay conexion con la base de datos");
+        }
+
+    });
+
+    $.ajax({
+
+        type: "POST",
+        url: "http://192.168.1.100/Saludapp/BackendFolder/consultarNotaIngreso3era.php",
+        data: {},
+        cache: false,
+        success: function (data) {
+            $("#tableNotaIngreso3era").replaceWith(data);
+        },
+        error: function (data) {
+            alert("No hay conexion con la base de datos");
+        }
+
+    });
+
+}

6514a2c4d5a45b9d02d029ab40b4f9169fca63b6
AdminCarlos <carlosflores92.cf@gmail.com>
2019-11-09 13:30:47 -0600

    feat: Agregadas controles para la funcion obtenernotaIngreso

--- Saludapp Movil/www/scripts/index.js
+++ Saludapp Movil/www/scripts/index.js
@@ -11,6 +11,7 @@
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
+        document.addEventListener("deviceready", initializeControls, false);
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
@@ -27,4 +28,13 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
+
+function initializeControls() {
+
+    $("#buttonObtenerNota").click(function() {
+        obtenerNotaIngreso();        
+    });
+
+}
+
